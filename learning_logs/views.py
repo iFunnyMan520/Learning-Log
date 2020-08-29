718491725aca2f3709c72797e953bdca9f57132b
iFuunyMan520 <ifuunyman520@gmail.com>
2020-08-29 18:00:09 +0300

    create home page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """Home page Learning Log app"""
+    return render(request, 'learning_logs/index.html')
